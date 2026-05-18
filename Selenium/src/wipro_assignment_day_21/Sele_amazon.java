e7e1ea967dd58ca8417993d9e637ad245ff37ff0
Deepu-Jha <narendrajha074@gmail.com>
2026-05-18 10:57:59 +0530

    day_21

---
+++ Selenium/src/wipro_assignment_day_21/Sele_amazon.java
@@ -0,0 +1,93 @@
+package wipro_assignment_day_21;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sele_amazon {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.in");
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("nav-link-accountList")).click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("ap_email_login"))
+              .sendKeys("7033474266");
+
+        driver.findElement(By.id("continue")).click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("ap_password"))
+              .sendKeys("User@1234");
+
+        driver.findElement(By.id("signInSubmit")).click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("twotabsearchtextbox"))
+              .sendKeys("Laptop Charger");
+
+        driver.findElement(By.id("nav-search-submit-button"))
+              .click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.partialLinkText("Laptop Charger"))
+              .click();
+
+        Thread.sleep(5000);
+
+        for (String windowHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(windowHandle);
+        }
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("add-to-cart-button"))
+              .click();
+
+        Thread.sleep(5000);
+
+        driver.get("https://www.amazon.in");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("twotabsearchtextbox"))
+              .sendKeys("Phone Charger");
+
+        driver.findElement(By.id("nav-search-submit-button"))
+              .click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.partialLinkText("Phone Charger"))
+              .click();
+
+        Thread.sleep(5000);
+
+        for (String windowHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(windowHandle);
+        }
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("add-to-cart-button"))
+              .click();
+
+        Thread.sleep(5000);
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
